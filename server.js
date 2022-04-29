58dedbce8898bcd9865b7ba944e941ea76df9cc7
nehachandra27 <65874523+nehachandra27@users.noreply.github.com>
2022-04-29 14:49:19 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("./app/models");
+const app = express();
+var corsOptions = {
+  origin: "http://localhost:4200"
+};
+
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to puzzle application." });
+  db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+});
+// set port, listen for requests
+
+require("./app/routes/winners.routes")(app);
+const PORT = process.env.PORT || 8090;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
